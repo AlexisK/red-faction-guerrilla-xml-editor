0e97674755be80949644d1f4da3738865616a89d
Oleksii Kaliuzhnyi <alexey@stuccomedia.com>
2017-03-15 16:53:39 +0200

    add support for entities

--- script.js
+++ script.js
@@ -44,7 +44,7 @@
 
     function readTextFile(file, cb) {
         var rawFile = new XMLHttpRequest();
-        rawFile.open("GET", './'+file, false);
+        rawFile.open("GET", './' + file, false);
         rawFile.onreadystatechange = function () {
             if ( rawFile.readyState === 4 ) {
                 if ( rawFile.status === 200 || rawFile.status == 0 ) {
@@ -99,30 +99,36 @@ var WORKERS = {
     //weapon : data => representRef('weapon: ' + data.name, data)
 };
 
-function generateWorker(type, nameKey, fieldsAvailable) {
-    fieldsAvailable = fieldsAvailable.split(',');
-    WORKERS[type]   = data => {
-        console.log(data);
-        let result = {};
-        for (let k in data) {
-            if ( fieldsAvailable.indexOf(k) >= 0 ) {
-                result[k] = data[k];
-            }
-        }
-        representRef(type + ': ' + data[nameKey], result)
-    }
-}
-
-generateWorker('weapon', 'name', 'ammo_type,magazine_size,max_rounds,damage_scaling_min,damage_scaling_max');
 
 // processing functions
 {
+
+    function generateWorker(type, fieldsAvailable = null, nameKey = 'name') {
+        WORKERS[type] = data => {
+            //console.log(data);
+
+            if ( !fieldsAvailable ) {
+                representRef(type + ': ' + data[nameKey], data)
+            } else {
+                if ( fieldsAvailable.split ) { fieldsAvailable = fieldsAvailable.split(','); }
+                let result = {};
+                for (let k in data) {
+                    if ( fieldsAvailable.indexOf(k) >= 0 ) {
+                        result[k] = data[k];
+                    }
+                }
+                representRef(type + ': ' + data[nameKey], result);
+            }
+
+        }
+    }
+
     function representRef(title, ref, target) {
         let parent            = createBlock(target || workBlock);
         let nodeTitle         = createBlock(parent);
         nodeTitle.textContent = title;
 
-        let valuesBlock               = createBlock(parent);
+        let valuesBlock = createBlock(parent);
         parent.classList.add('representation');
         nodeTitle.classList.add('title');
         valuesBlock.classList.add('values-group');
@@ -162,7 +168,7 @@ generateWorker('weapon', 'name', 'ammo_type,magazine_size,max_rounds,damage_scal
         target = target || createBlock();
         if ( typeof(data) === 'object' ) {
             for (let k in data) {
-                let key = k.split('#')[0];
+                let key     = k.split('#')[0];
                 let newNode = document.createElement(CONST.tagPrefix + key);
                 target.appendChild(newNode);
                 renderXMLNodeFromDict(data[key], newNode);
@@ -201,6 +207,13 @@ generateWorker('weapon', 'name', 'ammo_type,magazine_size,max_rounds,damage_scal
         renderResult();
     }
 }
+
+
+generateWorker('item');
+generateWorker('gamestatedata');
+generateWorker('weapon', 'ammo_type,magazine_size,max_rounds,damage_scaling_min,damage_scaling_max');
+generateWorker('explosion', 'ai_sound_radius,radus,crumbleradius,knockdownradius,flinchradius,impulse,human_damage_min,human_damage_max,vehicle_damage_min,vehicle_damage_max,structural_damage');
+
 
 textInput.onblur = processInput;
 textInput.value  = '<root><Table><Weapon> <Name>singularity_bomb</Name> <Weapon_class>singularity_bomb</Weapon_class> <Trigger_type>single</Trigger_type> <Ammo_type>thrown</Ammo_type> <Magazine_size>1</Magazine_size> <Max_rounds>2</Max_rounds> <Range_max>35</Range_max> <Range_red>5</Range_red> <Default_refire_delay>2000</Default_refire_delay> <Damage_scaling_max> <Npc_damage>2000</Npc_damage> <Player_damage>2000</Player_damage> <Vehicle_damage>-1</Vehicle_damage> <Player_vehicle_damage>-1</Player_vehicle_damage> </Damage_scaling_max> <Damage_min_dist>10</Damage_min_dist> <_editor> <Category>Entries:MP Overrides</Category> </_editor> <Animation_group>remote charge</Animation_group> <Min_engagement_distance>10</Min_engagement_distance> <Max_engagement_distance>20</Max_engagement_distance> <Inventory_item>singularity_bomb</Inventory_item> <Item_3d>singularity_bomb</Item_3d> <Npc_firing_pattern>Other</Npc_firing_pattern> <Ammo_box_restock>0</Ammo_box_restock> <Num_magazines>2</Num_magazines> <Max_ai_penetrating_distance>40.0</Max_ai_penetrating_distance> <Dummy>False</Dummy> <Reticule_name>ui_hud_reti_singularitybomb</Reticule_name> <Unique_id>105</Unique_id> <Icon_name>ui_hud_weapon_icon_singbomb</Icon_name> <Default_team>EDF</Default_team> <Aim_assist>0.5</Aim_assist> </Weapon> </Table></root>';
